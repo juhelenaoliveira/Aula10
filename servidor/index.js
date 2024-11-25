a397c6ef1de92666e457f63cb66fa3810f0f02f2
juhelenaoliveira <juliahelenaoliveira13@gmail.com>
2024-11-25 09:42:54 -0300

    25/11

---
+++ servidor/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'aulabd'
+});
+
+//Conectando ao banco de dados
+db.connect((erro) => {
+    if (erro) {
+        console.error('Erro ao conectar ao MySQL:', erro);
+    } else {
+        console.log('Conectado ao MySQL com sucesso!');
+    }
+});
+
+// Rota para cadastar usuário
+app.post('/alunos', (req, res) => {
+    const { nome, cidade, estado } = req.body;
+
+    const sql = 'INSERT INTO alunos (nome, cidade, estado) VALUES (?, ?, ?)';
+
+    db.query(sql, [nome, cidade, estado], (err, result) => {
+        if (err)
+        {
+            return res.status(500).json({ error: 'Erro ao cadastrar aluno !' }); 
+        }
+        res.status(201).json({ message: 'Aluno cadastrado com sucesso!', id: result.insertId });
+    });
+});
+
+// Rota para consultar todos os usuários
+app.get('/alunos', (req, res) => {
+    const sql = 'SELECT * FROM alunos';
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Erro ao consultar alunos' });
+        }
+        res.json(results);
+    });
+});
+
+//Iniciando o servidor 
+app.listen(PORT, () => {
+    console.log('Servidor rodadndo em http://localhost:${PORT}');
+});
